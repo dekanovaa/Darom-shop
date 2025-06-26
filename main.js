40d3b453f4d5b85abf52c522aaf43071fa1b2bce
dekanovaa <157375009+dekanovaa@users.noreply.github.com>
2025-06-26 23:57:27 +0500

    vdf

--- main.js
+++ main.js
@@ -287,3 +287,18 @@ document.getElementById('phone').addEventListener('input', function(e) {
 updateStepCounter();
 updateProgressBar();
 updateNavigation();
+
+
+// scroll uchun
+
+const scrollBtn = document.getElementById("scrollToTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollBtn.classList.remove("opacity-0", "pointer-events-none");
+    scrollBtn.classList.add("opacity-100");
+  } else {
+    scrollBtn.classList.add("opacity-0", "pointer-events-none");
+    scrollBtn.classList.remove("opacity-100");
+  }
+});
